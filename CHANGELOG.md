d74c2c7c5a3858f9f117c68811720f451f023959
hex7c0 <carniellifrancesco@gmail.com>
2018-01-09 22:27:59 +0100

    0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+v0.2.0-public / 2018-01-09
+==================
+
+ * (build) repo is public
+ * (build) minor fixes
+
+v0.1.0-go-go / 2017-12-10
+==================
+
+ * (build) compiled with 1.9.2
+ * (configs) try to grab gdo configuration
+ * (libs) resend message to Queue 1 time
--- file/log.go
+++ file/log.go
@@ -11,8 +11,8 @@ import (
 )
 
 const (
-	LogExtension = ".log"
-	NewLine      = '\n'
+	LogExtension = ".log" // log extension
+	NewLine      = '\n'   // log newline
 )
 
 // Log is a single transaction by timestamp.
@@ -25,7 +25,7 @@ var (
 	timestampRegex = regexp.MustCompile(`[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}`)
 )
 
-// Try to detect timestamp from body with regex and time parser
+// Try to detect timestamp from body with regex and time parser.
 // [2017-12-01 23:50:07]
 // [2017-12-01 23:50:10]
 func detectTimestamp(line []byte) (timestampDetected time.Time) {
